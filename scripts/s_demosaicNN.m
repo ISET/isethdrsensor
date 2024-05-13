de4ac473ad29cec99ceeba6852d023d0cafcc4df
Brian Wandell <1837145+wandell@users.noreply.github.com>
2024-05-13 14:33:42 -0700

    Ignore the networks/directory

--- scripts/s_demosaicNN.m
+++ scripts/s_demosaicNN.m
@@ -1,4 +1,25 @@
+%% Demosaic with a trained Restormer network
+%
+% 
 
+%% Set up ISET and the Python environment
+ieInit;
+
+% Each user would have this somewhere in their own compute environment
+%{
+In the terminal, you need to set up the environment to have the
+methods that are necessary to run the PyTorch model.
+
+  conda activate py39
+  conda install --yes --file utility/python/requirements.txt
+%}
+pe = pyenv('Version','/Users/wandell/opt/anaconda3/envs/py39/bin/python');
+
+%% We need a scene and an OI
+%
+%{
+  scene = sceneFromFile('Feng_Office-hdrs.mat',???)
+%}
 oi = oiCreate('wvf');
 % oi = oiSet(oi,'optics model','shift invariant');
 
@@ -9,18 +30,23 @@ wvf    = wvfCreate('spatial samples', 1024);
 
 oi = oiSet(oi,'fnumber',1.7);
 oi = oiSet(oi,'focal length',4.38e-3,'m');
+
 oi = oiCompute(oi, scene,'crop',true,'pixel size',1.5e-6,'aperture',aperture);
+
+% oiWindow(oi);
+
+%%
 [ip,sensor] = piRadiance2RGB(oi,'sensor','ar0132atSensorRGBW', ...
     'etime',1/200,'noiseflag',2, ...
     'pixelsize',1.5,'analoggain',1/20);
 
+%% Set up the demosaicing
+
 sensorEXR_path = fullfile(isethdrsensorRootPath,'local','tmp.exr');
-
-[fname,rgbw] = sensor2EXR(sensor,sensorEXR_path,'data type','volts','data format','noisyrgb','noise level',0.01);
-%%
-output_path = fullfile(isethdrsensorRootPath,'local','tmp_output.exr');
-
+[fname,rgbw]   = sensor2EXR(sensor,sensorEXR_path,'data type','volts','data format','noisyrgb','noise level',0.01);
+output_path    = fullfile(isethdrsensorRootPath,'local','tmp_output.exr');
 [result, output_path] = isetDemosaicNN('rgbw', sensorEXR_path, output_path);
+
 %%
 [ip,sensor] = piRadiance2RGB(oi,'sensor','ar0132atSensorRGB', ...
     'etime',1/200,'noiseflag',2, ...
