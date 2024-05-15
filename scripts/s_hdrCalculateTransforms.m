d9f7f1f451a4cd6c2593622505ee68a322c4bcab
Brian Wandell <1837145+wandell@users.noreply.github.com>
2024-05-15 16:15:27 -0700

    Update s_hdrCalculateTransforms.m

--- scripts/s_hdrCalculateTransforms.m
+++ scripts/s_hdrCalculateTransforms.m
@@ -8,12 +8,22 @@ oi = oiCompute(oi,scene);
 % We should be able to find T a simpler way and embed that into the
 % 'transform method','rgbw restormer'.  Here we set the noise to zero
 % because it appears that T depends on the data in some way.
-sensorRGB = sensorCreate('ar0132at',[],'rgb');
-sensorRGB = sensorSet(sensorRGB,'noise flag',0);
-sensorRGB = sensorCompute(sensorRGB,oi);
-                
+% sensorRGB = sensorCreate('ar0132at',[],'rgb');
+% sensorRGB = sensorSet(sensorRGB,'noise flag',0);
+% sensorRGB = sensorCompute(sensorRGB,oi);
+% 
+
 ip = ipCreate;
-T{1} = ieColorTransform(sensorRGB,'XYZ','D65','mcc');
+sensorRGBW = sensorCreate('ar0132at',[],'rgbw');
+
+% These match!  So write a routine to get the transforms based on the
+% RGB of the RGBW sensor.  No need to create the RGB and run an
+% ipCompute to calculate the transforms.
+wave     = sensorGet(sensorRGBW,'wave');
+sensorQE = sensorGet(sensorRGBW,'spectral qe');
+targetQE = ieReadSpectra('xyzQuanta',wave);
+T{1} = imageSensorTransform(sensorQE(:,1:3),targetQE,'D65',wave,'mcc');
+% T{1} = ieColorTransform(sensorRGB,'XYZ','D65','mcc');
 T{2} = eye(3,3);
 T{3} = ieInternal2Display(ip);
 ip = ipSet(ip,'transforms',T);
