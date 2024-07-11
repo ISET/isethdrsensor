1db6440640a70ff03c19ac8cf0704bc6575fdde2
Brian Wandell <wandell@stanford.edu>
2024-07-10 21:22:31 -0700

    Update s_hsSceneCreate.m

--- scripts/s_hsSceneCreate.m
+++ scripts/s_hsSceneCreate.m
@@ -5,23 +5,34 @@
 % function scene = hsSceneCreate(imageID,lstDir,dynamicRange,lowLight);
 %
 
+%%  These are all the scenes BW processed.
+
+% We need a different version of this, probably threw Andrew's database.
 lst = hsSceneDescriptions('print',false);
 
-imageID = lst(7).id;
-% lstDir = '/Volumes/TOSHIBA EXT/isetdata/lightgroups';
-lstDir = '/Volumes/Wandell/Data/lightgroups';
+%%
+% '1112201236' - Good one
+
+imageID = lst(5).id;
+% lstDir = '/Volumes/TOSHIBA EXT/isetdata/lightgroups';  % Office disk
+lstDir = '/Volumes/Wandell/Data/lightgroups';  % Home disk
 
 fname = fullfile(lstDir,sprintf('HDR-scenes-%s',imageID));
 load(fname,'scenes','sceneMeta');
 
+%%
 dynamicRange = 10^5;
 lowLight = 10;
 scene = lightGroupDynamicRangeSet(scenes, dynamicRange, lowLight);
+
+%% Edit the scene by cropping and denoising
 
 % Crop here ..
 
 % Denoise here
 scene = piAIdenoise(scene);
+
+%% Adjust scene parameters and show in window
 
 scene = sceneSet(scene,'fov',40);   % I cropped the big scene down.
 scene = sceneSet(scene,'depth map',sceneMeta.depthMap);
@@ -30,3 +41,31 @@ scene = sceneSet(scene,'metadata',metadata);
 sceneWindow(scene);
 
 %%
+%{
+[oi,wvf] = oiCreate('wvf');
+[aperture, params] = wvfAperture(wvf,'nsides',3,...
+    'dot mean',50, 'dot sd',20, 'dot opacity',0.5,'dot radius',5,...
+    'line mean',50, 'line sd', 20, 'line opacity',0.5,'linewidth',2);
+
+oi = oiSet(oi,'wvf zcoeffs',0,'defocus');
+oi = oiCompute(oi, scene,'aperture',aperture,'crop',true, 'pixel size',3e-6);
+%}
+%{
+% Plotting this scene with oi gamma set to 0.3;
+% 1112201236
+oiWindow(oi);
+oi = oiSet(oi,'gamma',0.3);
+[udata, hdl ] = oiPlot(oi,'hline illuminance',[1,564]);
+set(gca,'yscale','log');
+rgb = oiGet(oi,'rgb');
+[r,c,w] = size(rgb);
+ieNewGraphWin; imagesc(rgb); axis image;
+hold on;
+thisL = line([1 1925],[564 564],'Color','g','LineStyle','--');
+thisL.LineWidth = 0.1;
+yyaxis right;
+plot(1:numel(udata.data),udata.data,'w-');
+ax = gca; ax.YAxis(2).Scale = 'log'; ax.YAxis(2).Limits = [10^-2,10^11];
+ylabel('Log10 Illuminance');
+% Position:   0.0070    0.3986    0.4825    0.5114
+%}
