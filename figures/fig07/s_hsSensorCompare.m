7e03be94377bd776603952d8ed1f0c6f418021f7
Brian Wandell <wandell@stanford.edu>
2024-07-28 15:30:10 -0700

    Update s_hsSensorCompare.m

--- figures/fig07/s_hsSensorCompare.m
+++ figures/fig07/s_hsSensorCompare.m
@@ -18,39 +18,16 @@
 % See also
 %   s_hsSplitPixelParameters
 
-%%
-ieInit;
+%% Notes on Scene Creation
+%
+% At this time, people need to get oiNight manually for this to work. We
+% will find a way to put it in an open, downloadable, site.
+%
 
-% imageID = '1112201236'; % - Good one
-imageID = '1114091636';   % Red car, green car.  Used in paper.
-
-%% Day scene weights
-
-% sceneWindow(scene,'render flag','clip');
-
-%% Create the optics
-[oi,wvf] = oiCreate('wvf');
-params = wvfApertureP;
-% We should implement wvfApertureSet/Get so we do not have to remember
-% the parameter names precisely.
-% {
-params.nsides = 3;
-params.dotmean = 50;
-params.dotsd = 20;
-params.dotopacity =0.5;
-params.dotradius = 5;
-params.linemean = 50;
-params.linesd = 20;
-params.lineopacity = 0.5;
-params.linewidth = 2;
-%}
-
-aperture = wvfAperture(wvf,params);
-oi = oiSet(oi,'wvf zcoeffs',0.1,'defocus');   % Slight defocus.  Not sure why.
-
-%%  If you want the oiDay, this is how
+%%  This is how we created oiDay
 
 %{
+imageID = '1114091636';   % Red car, green car.  
 wgts = [0    0     0    100*0.5175]; % Day
 scene = hsSceneCreate(imageID,'weights',wgts,'denoise',true);
 oiDay = oiCompute(oi, scene,'aperture',aperture,'crop',true,'pixel size',3e-6);
@@ -61,14 +38,10 @@ save(oiName,'oiDay','-v7.3');
 
 %}
 
-%% Night scene weights
-
-% For final, remember to turn off denoise
-
-% Experimenting with how dark.  4 log units down gets night
-% But three really doesn't.
+%% This is how we created oiNight
 
 %{
+imageID = '1114091636';   % Red car, green car.  
 wgts    = [0.2306    0.0012    0.0001    1e-2*0.5175]; % Night
 scene   = hsSceneCreate(imageID,'weights',wgts,'denoise',true);
 oiNight = oiCompute(oi, scene,'aperture',aperture,'crop',true,'pixel size',3e-6);
@@ -78,54 +51,81 @@ oiName = fullfile(isethdrsensorRootPath,'local',sprintf('oiNight-%s.mat',imageID
 save(oiName,'oiNight','-v7.3');
 %}
 
-%% Standard automotive rgb
-
-% To speed things up
-% {
- oiName = fullfile(isethdrsensorRootPath,'local',sprintf('oiNight-%s.mat',imageID));
- load(oiName,'oiNight');
- oiInput = oiNight;
-%}
-%{
- oiName = fullfile(isethdrsensorRootPath,'local',sprintf('oiDay-%s.mat',imageID));
- load(oiName,'oiDay');
- oiInput = oiDay;
-%}
 
 %%
-expTime = 16e-3;   % 16e-3 is 60 hz.
+ieInit;
+
+% Used in paper.
+imageID = '1114091636';   % Red car, green car.  
+
+% An option for some other time.
+% imageID = '1112201236'; % - Good one
+
+oiName = fullfile(isethdrsensorRootPath,'local',sprintf('oiNight-%s.mat',imageID));
+load(oiName,'oiNight');
+oiInput = oiNight;
+
+%% Create the optics
+[oi,wvf] = oiCreate('wvf');
+
+% Choose some aperture properties.  These determine the flare and blur.
+params = wvfApertureP;
+params.nsides = 3;
+params.dotmean = 50;
+params.dotsd = 20;
+params.dotopacity =0.5;
+params.dotradius = 5;
+params.linemean = 50;
+params.linesd = 20;
+params.lineopacity = 0.5;
+params.linewidth = 2;
+
+% Create the aperture
+aperture = wvfAperture(wvf,params);
+
+% Slight defocus.  Just a choice.
+oi = oiSet(oi,'wvf zcoeffs',0.1,'defocus');   
+
+%% Run a standard RGB sensor
+
+% 16e-3 is 60 h frame rate.  Used for all the captures below.
+expTime = 16e-3;   
+
+% Here is the standard sensor
 sensorRGB = sensorCreate('ar0132at',[],'rgb');
 sensorRGB = sensorSet(sensorRGB,'match oi',oiInput);
 sensorRGB = sensorSet(sensorRGB,'exp time',expTime);
 sensorRGB = sensorSet(sensorRGB,'noise flag',2);
 sensorRGB = sensorCompute(sensorRGB,oiInput);
+
 sensorWindow(sensorRGB,'gamma',0.3);
 
+% Save out the RGB image
 rgb = sensorGet(sensorRGB,'rgb');
 imName = sprintf('ar0132atSensor.png');
 imwrite(rgb,fullfile(isethdrsensorRootPath,'local',imName));
 
-%{ 
+%{
+% Have a close look, if you want.
 sensorShowImage(sensorRGB,sensorGet(sensorRGB,'gamma'),true,ieNewGraphWin);
 truesize
 %}
 
-%% Turn off the noise and recompute
+%% Turn off the noise, recompute, and show the noise.
 
-% For this scene ID:  1114091636
 whichLine = 859;   
-
 % whichLine = 142; % An interesting one, also
 
+% No noise.
 sensorRGB2 = sensorSet(sensorRGB,'noise flag',0);
 sensorRGB2 = sensorSet(sensorRGB2,'name','no noise');
 sensorRGB2 = sensorCompute(sensorRGB2,oiInput);
 % sensorWindow(sensorRGB2,'gamma',0.3);
 
-% sensorPlot(sensorRGB2,'volts hline',[1 whichLine], 'two lines',true);
-% sensorPlot(sensorRGB,'volts hline',[1 whichLine], 'two lines',true);
-uDataRGB = sensorPlot(sensorRGB,'volts hline',[1 whichLine],'no fig',true);
+uDataRGB  = sensorPlot(sensorRGB,'volts hline',[1 whichLine],'no fig',true);
 uDataRGB2 = sensorPlot(sensorRGB2,'volts hline',[1 whichLine],'no fig',true);
+
+% The red channel
 channel = 1;
 x = uDataRGB.data{channel};
 y = uDataRGB2.data{channel};
@@ -143,22 +143,20 @@ title('1-capture (ar0132at)');
 tmp = sprintf('rgb-%d-noise.pdf',whichLine);
 exportgraphics(gcf,fullfile(isethdrsensorRootPath,'local',tmp));
 
+%% Calculate how closely the measurements track the no noise values
+
 % Assuming x and y are your data vectors
 X = [ones(length(x), 1), x];  % Add a column of ones for the intercept
 [b,~,~,~,stats] = regress(y, X);
 fprintf('RGB R_squared" %f\n',stats(1));
 
-%% Split pixel , night time.  Default parameters.
+%% Now the Omnivision (OVT) Split pixel technology.
 
 pixelSize = sensorGet(sensorRGB,'pixel size');
 sensorSize = sensorGet(sensorRGB,'size');
+arrayType = 'ovt'; 
 
-arrayType = 'ovt'; % imx490 ovt
-
-% IMX490 or OVT
-% I ran both.  The IMX490 does well.  The OVT design, not as well.  I think
-% that is interesting.  3-capture vs. 4-capture.  The additional HCG in the
-% small pixel picks up the dark region!
+% The OVT design is a 3-capture (two large PD captures and one small PD).
 sensorArray = sensorCreateArray('array type',arrayType,...
     'pixel size same fill factor',pixelSize,...
     'exp time',expTime, ...
@@ -171,12 +169,13 @@ sensorWindow(sensorSplit,'gamma',0.3);
 % We probably need to reset gamma to 1 before these sensorGet calls
 rgb = sensorGet(sensorSplit,'rgb');
 
+imName = sprintf('splitSensor.png');
+imwrite(rgb,fullfile(isethdrsensorRootPath,'local',imName));
+
 %{ 
 sensorShowImage(sensorSplit,sensorGet(sensorSplit,'gamma'),true,ieNewGraphWin);
 truesize
 %}
-imName = sprintf('splitSensor.png');
-imwrite(rgb,fullfile(isethdrsensorRootPath,'local',imName));
 
 %% Turn off the noise and compare
 
@@ -188,8 +187,6 @@ sensorArray = sensorCreateArray('array type',arrayType,...
     'noise flag',0);
 [sensorSplit2, sensorArraySplit2] = sensorComputeArray(sensorArray,oiInput,'method','saturated');
 
-% sensorPlot(sensorSplit2,'volts hline',[1 whichLine], 'two lines',true);
-% sensorPlot(sensorSplit,'volts hline',[1 whichLine], 'two lines',true);
 uDataRGB = sensorPlot(sensorSplit,'volts hline',[1 whichLine],'no fig',true);
 uDataRGB2 = sensorPlot(sensorSplit2,'volts hline',[1 whichLine],'no fig',true);
 
@@ -219,10 +216,11 @@ exportgraphics(gcf,fullfile(isethdrsensorRootPath,'local',tmp));
 X = [ones(length(x), 1), x];  % Add a column of ones for the intercept
 [b,~,~,~,stats] = regress(y, X);
 fprintf('Split R_squared" %f\n',stats(1));
+
 % slope = b(2)
 % intercept = b(1)
 
-%% image process?
+%% Image process the RGB and split pixel
 
 ipRGB = ipCreate;
 ipRGB = ipCompute(ipRGB,sensorRGB);
@@ -238,21 +236,22 @@ rgb = ipGet(ipSplit,'srgb');
 fname = fullfile(isethdrsensorRootPath,'local','ip-split.png');
 imwrite(rgb,fname);
 
-%% Show the narrowing around the lights
+%% Show the improved spatial representation near the headlights
 
 % 626 row is bottom two lights  150 - 600
 % 586 row is next lights up 250 900
 % 551 row the distant cars - 1000 1200
 % 296 row one of the upper lights  - 600 800
 uSplit = ipPlot(ipSplit,'horizontal line luminance',[1 626],'no figure');
-uRGB = ipPlot(ipRGB,'horizontal line luminance',[1 626],'no figure');
+uRGB   = ipPlot(ipRGB,'horizontal line luminance',[1 626],'no figure');
 
 ieNewGraphWin([],'wide');
-plot(uSplit.pos,ieScale(uSplit.data,1),'k-',uRGB.pos,ieScale(uRGB.data,1),'ko:','LineWidth',2);
+plot(uSplit.pos,ieScale(uSplit.data,1),'k-',...
+    uRGB.pos,ieScale(uRGB.data,1),'ko:','LineWidth',2);
 legend({'split','rgb'});
-grid on;
-set(gca,'xlim',[150 600])
+grid on; set(gca,'xlim',[150 600]); 
 xlabel('Column'); ylabel('Relative luminance');
+
 tmp = sprintf('split-ar0132at-luminance.pdf');
 exportgraphics(gcf,fullfile(isethdrsensorRootPath,'local',tmp));
 
@@ -270,20 +269,21 @@ img = sensorShowImage(sensorSplit,gam,scaleMax,0);
 tmp = sprintf('%s.png',sensorGet(sensorSplit,'name'));
 imwrite(img,fullfile(isethdrsensorRootPath,'local',tmp));
 
-%%
-%{
-ieNewGraphWin; imagesc(sensorSplit.metadata.bestPixel);
-colormap([1,0,0; 0,1,0; 0,0,1]);
+%% Some potential graphs
 
-histogram(sensorSplit.metadata.bestPixel(:));
-%}
-%{
+% In most places, we can average the data from the two LPD captures
+% There is only a small region where 2 is captured and 1 is not.
+% In the region where 1 is saturated, 2 is also saturated.  There, we use
+% the SPD data.
 saturated = sensorSplit.metadata.saturated;
 
-% Where is (a) 1 is saturated, and 
+% Where is (a) 1 and 2 are saturated
 %          (b) 1 is not saturated, but 2 is saturated
 ieNewGraphWin;
 tiledlayout(2,1);
-nexttile; imagesc(saturated(:,:,1));
-nexttile; imagesc(~saturated(:,:,1) .* saturated(:,:,2));
-%}
+nexttile; imagesc(saturated(:,:,1) .* saturated(:,:,2)); axis image;
+subtitle('Image 1 and 2 are saturated')
+nexttile; imagesc(~saturated(:,:,1) .* saturated(:,:,2)); axis image;
+subtitle('Image 2 saturated (but not 1)')
+
+%% END
