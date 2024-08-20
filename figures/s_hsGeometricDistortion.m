b8cac5a58faf55f17c8a48610e3b2d2d4ae0bd77
Zhenyi Liu <zhenyi27@stanford.edu>
2024-08-20 16:51:42 -0700

    Update s_hsGeometricDistortion.m

--- figures/s_hsGeometricDistortion.m
+++ figures/s_hsGeometricDistortion.m
@@ -1,14 +1,18 @@
 ieInit
+load('/Users/zhenyi/git_repo/dev/isethdrsensor/local/fig06_tunnel_oi.mat','oiInput');
+scene = piSceneCreate(oiInput.data.photons);
 
-scene = sceneCreate('hdr image');
-scene = sceneSet(scene,'fov',20);
-sceneWindow(scene,'renderflag','hdr');
+scene = piSceneCreate(oiInput.data.photons);
+scene = sceneSet(scene,'fov',170);
 
 %%
 % Set up default optical image
 oi = oiCreate;
-
-opticsFileName = fullfile(isetRootPath,'data','optics','zmWideAngle.mat');
+% opticsFileName = fullfile(isetRootPath,'data','optics','zemaxDoubleGauss.mat');
+% opticsFileName = fullfile(isetRootPath,'data','optics','zemaxCookeTriplet.mat');
+% opticsFileName = fullfile(isetRootPath,'data','optics','zemaxWideAngle.mat');
+opticsFileName = fullfile(isetRootPath,'data','optics','zemaxFisheye.mat');
+% opticsFileName = fullfile(isetRootPath,'data','optics','zmWideAngle.mat');
 load(opticsFileName,'optics');
 
 % Set the oi with the optics loaded from the file
@@ -25,7 +29,7 @@ oi = oiSet(oi,'wavelength',sceneGet(scene,'wavelength'));
 
 % Match the scene distance and the rt distance.  They are both essentially
 % infinite.
-scene = sceneSet(scene,'distance',2);  % Two meters - essentially inf
+scene = sceneSet(scene,'distance',1);  % Two meters - essentially inf
 oi    = oiSet(oi,'optics rtObjectDistance',sceneGet(scene,'distance','mm'));
 
 %% Compute the distortion and show it in the OI
@@ -43,8 +47,14 @@ if sFOV > oFOV
 end
 
 % scene = sceneInterpolateW(scene,(550:100:650));  % Small wavelength sample
+% oi.wAngular = 120;
 
 oi = rtGeometry(oi,scene,6);
+
+
+% 
+
+
 
 oiWindow(oi,'render flag','hdr');
 
