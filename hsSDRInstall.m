e2a3ebfe2508326282c4eed10abe50bc67a3a1b8
Brian Wandell <1837145+wandell@users.noreply.github.com>
2024-08-22 12:03:55 -0700

    Create hsSDRInstall.m

---
+++ hsSDRInstall.m
@@ -0,0 +1,19 @@
+%% This script (maybe function) will download and install from the SDR
+%
+% We will pull files into data and networks
+%
+% The overall site is here
+%
+%  https://searchworks.stanford.edu/view/bt316kj3589
+%
+% For retrieving data there is a base URL
+% 
+%   https://stacks.stanford.edu/file/druid:bt316kj3589/isethdrsensor/
+%
+% Specific data just the names of the directories and files, such as
+% 
+%   data/1112184733/1112184733.mat
+%
+% See also
+%
+
